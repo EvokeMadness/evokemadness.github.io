0ed57f29aa6ca88f0a9816ad6f685a3f0f3f5af2
Jason <antgonzales9643@gmail.com>
2026-01-21 20:39:55 -0600

    Finishing touches on home page

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,5 +2,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("content");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon.svg");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 };
